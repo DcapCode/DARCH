1ce99f6ee459794afa1c1d47e1240abb72c5f493
dcap <dcap@dcapedcode.com>
2026-01-25 22:15:26 +0100

    Add all config files from old setup

---
+++ .config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,9 @@
+return
+{
+	{
+		'catppuccin/nvim',
+		name = 'catppuccin',
+        priority = 1000
+	}
+	-- Add more themes here if wanted
+}
---
+++ .config/nvim/lua/plugins/threesitter.lua
@@ -0,0 +1,20 @@
+return
+{
+	{
+		'nvim-treesitter/nvim-treesitter',
+		build = function()
+			pcall(require('nvim-treesitter.install').update({ with_sync = true }))
+		end,
+		config = function()
+			-- require('nvim-treesitter.configs').setup({
+			--	ensure_installed = {'lua', 'json', 'yaml'},
+			--	highlight = { enable = true },
+			--	sync_install = false,
+			--	auto_install = true,
+			--	additional_vim_regex_highlighting = false,
+			--})
+		end
+
+	}
+
+}
